766d809b4414af45f3b2b17a54283dd3857a840e
Dewipangastuti <dewipanga@gmail.com>
2024-02-12 11:06:40 +0700

    tambah docstring

--- penjumlahan.py
+++ penjumlahan.py
@@ -1,4 +1,14 @@
 def penjumlahan(angka_1, angka_2):
+    """
+    Menampilkan penjumlahan dari 2 angka
+
+    Args:
+        angka_1 (int): _description_
+        angka_2 (int): _description_
+
+    Returns:
+        penjumlahan: _description_
+    """
     jumlah = angka_1 + angka_2
     return jumlah
 
